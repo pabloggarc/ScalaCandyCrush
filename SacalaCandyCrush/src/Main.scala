95fa2f491c168cb753ad103bf530aeb06677e0b9
alexanderbrung <78935883+alexanderbrung@users.noreply.github.com>
2023-04-04 20:52:46 +0200

    error en llamada a mostrar tablero

--- SacalaCandyCrush/src/Main.scala
+++ SacalaCandyCrush/src/Main.scala
@@ -1,10 +1,6 @@
 import scala.util.Random
 object Main {
 
-  var dificultad: Int = -1
-  var modo: Int = -1
-  var N: Int = 0
-  var M: Int = 0
 
   def cargarArgumentos(args: Array[String]): Unit = {
 
@@ -14,17 +10,17 @@ object Main {
     } else {
       var error = false
       if (args(1) == "-a") {
-        modo = 0
+        val modo = 0
       } else if (args(1) == "-m") {
-        modo = 1
+        val modo = 1
       } else {
         error = true
       }
 
       if (args(2) == "1") {
-        dificultad = 0
+        val dificultad = 0
       } else if (args(2) == "2") {
-        dificultad = 1
+       val dificultad = 1
       } else {
         error = true
       }
@@ -32,10 +28,9 @@ object Main {
       if (args(3) < "1" || args(4) < "1"){
         error = true
       } else{
-        N = args(3).toInt
-        M = args(4).toInt
+       val N = args(3).toInt
+       val M = args(4).toInt
       }
-
 
       if (error) {
         println("Valor de argumento inválido.")
@@ -47,7 +42,7 @@ object Main {
 
 
 
-  def generar_elemento(): Char = {
+  def generar_elemento(dificultad: Int): Char = {
     val r = new Random()
     if (dificultad == 1) {
       (r.nextInt(6 - 1 + 1) + 1).toChar
@@ -57,40 +52,41 @@ object Main {
   }
 
 
-  def mostrarTablero(tablero: Array[Char], n: Int, m: Int): Unit = {
+  def mostrarTablero(tablero: Array[Char], N: Int, M: Int): Unit = {
     def imprimirFila(fila: Array[Char], numFila: Int, filaStr: String): String = {
       if (fila == null || fila.size == 0) filaStr
       else imprimirFila(fila.tail, numFila, filaStr + " " + fila.head)
     }
 
-    def imprimirTablero(filas: Array[Array[Char]], numFila: Int, tableroStr: String): String = {
-      if (filas == null || filas.size == 0) tableroStr
+
+  def contarElementos(tablero: Array[Array[Char]]): Int = {
+    if (tablero == null) 0 // si la lista es nula o está vacía, devuelve un mensaje indicando que la lista está vacía
+    else if (tablero.tail == null) 1 // si la cola de la lista está vacía, significa que solo tiene un elemento
+    else 2 // en cualquier otro caso, la lista tiene más de un elemento
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    Random.setSeed(System.currentTimeMillis()) // semilla para la ejecución automática
+    cargarArgumentos(args)
+    val N: Int = 2
+    val M: Int = 2
+    val dificultad: Int = 1
+    val tam_tablero = N * M
+    val tablero = Array.tabulate(N, M)((i, j) => generar_elemento(dificultad)) // crear matriz y llenarla con la función generar_elemento
+
+    def medirVidas(lista: List[String], vidas: Int): Int = {
+      if (lista == null) vidas // caso base: si la lista es nula, devuelve el número de vidas restantes
+      else if (contarElementos(tablero) == 1){ medirVidas(lista.tail, vidas - 1)} // si el primer elemento de la lista tiene longitud 1, resta una vida y continúa con el resto de la lista
       else {
-        val filaStr = imprimirFila(filas.head, numFila, "")
-        imprimirTablero(filas.drop(1), numFila + 1, tableroStr + s"\n\t\t${numFila}\t${filaStr}")
+        mostrarTablero(tablero, N, M)
+
+
+
+        medirVidas(lista.tail, vidas)
       }
     }
 
-    def contar_elementos(arr: Array[Char]): Int = {
-      if (arr == null) 0
-      else if (arr.head == '\u0000') 0
-      else 1 + contar_elementos(arr.drop(1))
-    }
 
-    val filaBorde = (0 to m).map(_ => "-").mkString("   ")
-    val cabecera = (0 to m).map(i => f"${i}%3d").mkString("  ")
-
-    println("\nTABLERO: ")
-    println(s"\t\t   $cabecera")
-    println(s"\t\t   $filaBorde")
-    val filas = tablero.grouped(m).toArray
-    val tableroStr = imprimirTablero(filas, 0, "")
-    println(s"\t\t   $filaBorde")
-    println(tableroStr)
-  }
-
-
-  def main(args: Array[String]): Unit = {
-    println("Hello world!")
   }
 }
