c649721db4bc5fc9cea5543272090b2c74c00060
alexanderbrung <78935883+alexanderbrung@users.noreply.github.com>
2023-04-20 08:45:09 +0200

    Estrellas acabadas

--- Parte 3/ScalaCandyCrush/src/main/scala/ui/PanelEstrellas.scala
+++ Parte 3/ScalaCandyCrush/src/main/scala/ui/PanelEstrellas.scala
@@ -10,9 +10,9 @@ class PanelEstrellas extends FlowPanel {
 
   opaque = false
 
-  private val estrellaVacia: String = "src/img/VidaLlena.png"
-  private val estrellaLlena: String = "src/img/VidaVacia.png"
-  private val estrellaAzul: String = "src/img/Bomba.png"
+  private val estrellaVacia: String = "src/img/EstrellaVacia.png"
+  private val estrellaLlena: String = "src/img/EstrellaLlena.png"
+  private val estrellaAzul: String = "src/img/EstrellaAzul.png"
   private val estrellas: List[Label] = setEstrellas(3) // Lista de corazones
 
   private def setEstrellas(n: Int): List[Label]= { // Método que crea los corazones
@@ -39,20 +39,15 @@ class PanelEstrellas extends FlowPanel {
 
   private def actualizarEstrellas(puntos: Int, pos: Int, estr: List[Label]): Unit = {
 
-    println("voy a actualizar las estrellas y tengo estos puntos: "+ puntos)
     if (pos > 0) { // Si aún no se han actualizado todas las estrellas
       if (puntos >= 15000 && puntos < 30000) {
         estrellas(0).icon = new ImageIcon(new Imagenes(estrellaLlena).getImage.getScaledInstance(25, 25, Image.SCALE_REPLICATE))
 
-        println("Voy a cambiar la primera estrella")
       } else if (puntos >= 30000 && puntos < 45000) {
         estrellas(1).icon = new ImageIcon(new Imagenes(estrellaLlena).getImage.getScaledInstance(25, 25, Image.SCALE_REPLICATE))
-        println("Voy a cambiar la segunda estrella")
       } else if (puntos >= 45000 && puntos < 60000) {
         estrellas(2).icon = new ImageIcon(new Imagenes(estrellaLlena).getImage.getScaledInstance(25, 25, Image.SCALE_REPLICATE))
-        println("Voy a cambiar la tercera estrella")
       } else if (puntos >= 100000) {
-        println("Voy a poner las estrellas azules")
         estrellas(0).icon = new ImageIcon(new Imagenes(estrellaAzul).getImage.getScaledInstance(25, 25, Image.SCALE_REPLICATE))
         estrellas(1).icon = new ImageIcon(new Imagenes(estrellaAzul).getImage.getScaledInstance(25, 25, Image.SCALE_REPLICATE))
         estrellas(2).icon = new ImageIcon(new Imagenes(estrellaAzul).getImage.getScaledInstance(25, 25, Image.SCALE_REPLICATE))
